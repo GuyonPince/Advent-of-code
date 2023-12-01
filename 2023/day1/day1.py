0209c2b0a22b75b413fcf9e9cf99ccbe129a6d68
Guyon Pince van der Aa <guyon@delwi.nl>
2023-12-01 14:33:12 +0100

    Opgeschoond

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -7,39 +7,25 @@ text = ""
 with open("day1.txt") as file:
     text = file.read()
 
-
 #### PART 1a ###
-calibration_values_raw = []
-
+# Extract numbers using regex
+# running total
+calibration_val = 0
 for line in text.splitlines():
-    calibration_values_raw.append([int(x) for x in re.findall(r'\d',line)])
+    values = ([int(x) for x in re.findall(r'\d',line)])
 
-calibration_val1 = 0
-for values in calibration_values_raw:
     if len(values) > 1:
-        calibration_val1 += values[0]*10 + values[-1]
+        calibration_val += values[0]*10 + values[-1]
     else:
-        calibration_val1 += values[0]*11
+        calibration_val += values[0]*11
 
-### PART 1b ###
-# calibration_val1 = 0
-# for line in text.splitlines():
-#     first_digit = 0
-#     last_digit = 0
-#     for char in line:
-#         if not char.isdigit():
-#             continue
-#         if first_digit == 0:
-#             first_digit = int(char)
-#             last_digit = int(char)
-#         else:
-#             last_digit = int(char)
-    
-#     calibration_val1 += first_digit*10 + last_digit
-
+print("\nSolution part 1 = ", calibration_val)
+print("--- %s millis ---" % ((time.time() - start_time) * 1000))
 
 ### PART 2 ###
-calibration_val2 = 0
+# this time no regex, but check for digits in each line
+# keep track of only first and last digits. running total
+calibration_val = 0
 for line in text.splitlines():
     line = line.replace("one","o1e")
     line = line.replace("two","t2o")
@@ -51,9 +37,7 @@ for line in text.splitlines():
     line = line.replace("eight","e8t")
     line = line.replace("nine","n9e")
 
-
-    first_digit = 0
-    last_digit = 0
+    first_digit, last_digit = 0, 0
     for char in line:
         if not char.isdigit():
             continue
@@ -63,13 +47,10 @@ for line in text.splitlines():
         else:
             last_digit = int(char)
     
-    calibration_val2 += first_digit*10 + last_digit
-    # print (line, first_digit,last_digit)
+    calibration_val += first_digit*10 + last_digit
 
-
-print("Solution part 1 = ", calibration_val1)
-print("Solution part 2 = ", calibration_val2)
-print("--- %s millis ---" % ((time.time() - start_time) * 1000))
+print("\nSolution part 2 = ", calibration_val)
+print("--- %s millis ---\n" % ((time.time() - start_time) * 1000))
 
 ### Deel 2 
 # 55194 fout
