abfb5656572f2d7f61dfd8923501b17e6ce587f4
Guyon Pince van der Aa <guyon@delwi.nl>
2023-12-04 14:12:20 +0100

    added day 4, part 2

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -2,19 +2,47 @@ import re
 import time
 import numpy as np
 
+start_time = time.time()
+
 text = ""
 with open("day4.txt") as file:
     text = file.read()
 
-total_points = []
+class Card:
+    def __init__(self, id,numbers,winning_numbers) -> None:
+        self.id = id
+        self.numbers = numbers
+        self.winning_numbers = winning_numbers
+        self.copies = 1
+
+        self.wins = len(np.intersect1d(self.numbers,self.winning_numbers))
+        self.score = int(2**(self.wins-1))
+
+        
+cards = []
+# put all cards in a deck and set properties
 for line in text.splitlines():
-    x, card_numbers, winning_numbers = re.findall(r'[^\||:]+',line)
-    
-    card_numbers = [int(x) for x in re.findall(r'\d+',card_numbers)]
+    id, numbers, winning_numbers = re.findall(r'[^\||:]+',line)
+
+    id = [int(x) for x in re.findall(r'\d+',id)][0]
+    numbers = [int(x) for x in re.findall(r'\d+',numbers)]
     winning_numbers = [int(x) for x in re.findall(r'\d+',winning_numbers)]
+    cards.append(Card(id, numbers, winning_numbers))
 
-    overlap = np.intersect1d(card_numbers,winning_numbers)
-    points = int(2**(len(overlap)-1))
-    total_points.append(points)
+# iterate over the cards
+for i in range(len(cards)):
+    card = cards[i]
 
-print (sum(total_points))
+    # add copies of cards that you won
+    for j in range(i+1,i+card.wins+1):
+        if j > len (cards) - 1:
+            break
+        cards[j].copies += card.copies
+
+
+total_points = [x.score for x in cards] # part 1
+total_cards = [x.copies for x in cards] # part 2
+
+print("\nSolution part 1 = ",sum(total_points))
+print("Solution part 2 = ",sum(total_cards))
+print("--- %s millis ---\n" % ((time.time() - start_time) * 1000))
