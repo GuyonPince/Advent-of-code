80af95b40ca81e4d260891c4476c1d2d9f1cb2ee
Guyon Pince van der Aa <guyon@delwi.nl>
2023-12-05 22:37:28 +0100

    Day 5, part 1 now correct

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -9,58 +9,34 @@ text = ""
 with open("day5.txt") as file:
     text = file.read()
 
-class Seed_map:
-
-    def __init__(self, name, instructions):
-        self.name = name
-        self.instructions = instructions
-        self.destinations = []
-        self.sources = []
-        
-        self.create_list()
-
-    def create_list(self):
-        for i in self.instructions:
-            destination, source, steps = i
-
-            self.destinations.append(range(destination, destination + steps))
-            self.sources.append(range(source,source + steps))
-
-    def next_position(self, position):
-        for i,source in enumerate(self.sources):
-            for index, value in enumerate(source):
-                if position == value:
-                    return self.destinations[i][index]
-        return position
-
                 
-def find_endpos (seed):
-    position = seed
+def find_endpos (location):
     for map in maps:
-        position = map.next_position(position)
-    return position
-
+        for steps in map:
+            dest,source,n = steps
+            if source <= location < source+n:
+                location = dest+(location-source)
+                break
+    return location
 
 
 maps = []
 if __name__ == '__main__':
     seeds = []
 
-    chunks = text.split("\n\n")
-    seeds = [int(x) for x in re.findall(r'\d+',chunks[0])]
-    for chunk in chunks[1:]:
-        name = re.findall(r'(\w+-\w+-\w+)',chunk)[0]
+    seeds,*chunks = text.split("\n\n")
+    # seeds = list(map(int, seeds.split()[1:]))
+
+    for chunk in chunks:
         instructions = [[int(x) for x in i[1:]] for i in re.findall(r'((\d+) (\d+) (\d+))',chunk)]
+        maps.append(instructions)
 
-        maps.append(Seed_map(name,instructions))
+    final_locations = [find_endpos(s) for s in map(int,seeds.split()[1:])]
 
-    final_locations = []
-    with Pool(1) as p:
-        final_locations = p.map(find_endpos, seeds)
     
-        print("\nSolution part 1 = ",min(final_locations))
-        # print("Solution part 2 = ",sum(total_cards))
-        print("--- %s millis ---\n" % ((time.time() - start_time) * 1000))
+    print("\nSolution part 1 = ",min(final_locations))
+    # print("Solution part 2 = ",sum(total_cards))
+    print("--- %s millis ---\n" % ((time.time() - start_time) * 1000))
 
 
 # 4213184 is fout
