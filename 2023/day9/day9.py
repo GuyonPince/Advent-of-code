09705bc152df7212106b92adb0237be85532c15e
Guyon Pince van der Aa <guyon@delwi.nl>
2023-12-09 22:22:55 +0100

    Added day 8, part 1 and 2

--- 2023/day9/day9.py
+++ 2023/day9/day9.py
@@ -5,45 +5,39 @@ import numpy as np
 
 start_time = time.time()
 
-input = [[int(x) for x in lines.split()] for lines in open('sample.txt')]
+input = [[int(x) for x in lines.split()] for lines in open('input.txt')]
 
-def extrapolate(value_history, end_value,right):
-    diff = []
-    prev_val = 0
+def extrapolate(value_history,part):
+    prev_val, diff = 0, []
     for i,val in enumerate(value_history):
         if i > 0:
             diff.append(val-prev_val)
         prev_val = val
-
-    if right:
-        end_value += diff[-1]
-        print (value_history, end_value)
-    else:
-        end_value += diff[0]
-        print (diff[0],end_value,value_history)
-
     
     if not any(diff):
-        return (end_value)
+        return 0
     else:
-        return extrapolate(diff,end_value,right)
+        if part == 1:
+            return extrapolate(diff,part1) + diff[-1]
+        else:
+            return diff[0] - extrapolate(diff,part1)
 
 
 def part1():
     end_values = []
     for i in input:
-        end_values.append(i[-1]+extrapolate(i,0,True))
+        end_values.append(i[-1]+extrapolate(i,part=1))
 
     return sum(end_values)
 
 def part2():
     end_values = []
     for i in input:
-        end_values.append(extrapolate(i,0,False)-i[0])
+        end_values.append(i[0] - extrapolate(i,part=2))
 
     return sum(end_values)
 
 
 print("\nSolution part 1 = ",part1())
-print("Solution part 2 = ",part2())     # 912927543 = high
+print("Solution part 2 = ",part2())     # 385450922 = high
 print("--- %s millis ---\n" % ((time.time() - start_time) * 1000))
