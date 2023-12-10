b18279d82cd936c5a3eb0961ca6c307e6087a4b0
Guyon Pince van der Aa <guyon@delwi.nl>
2023-12-10 22:46:50 +0100

    Cleand day 8 part 1

--- 2023/day10/day10.py
+++ 2023/day10/day10.py
@@ -12,54 +12,44 @@ pipes = {'|':(0,0),
         'L':(-1,1),
         'J':(-1,-1),
         '7':(1,-1),
-        'F':(1,1)}
+        'F':(1,1),
+        'S':(0,0)}
 
 def find_start(grid):
+    '''Find the start position and direction of the pipe'''
     rows, collumns = np.shape(grid)
     for r in range (rows):
         for c in range (collumns):
             if grid[r,c] == 'S':
-                start_row, start_col =  r, c
+                pos =  r, c
                 break
-
-    if start_row > 0:
-        if grid[start_row-1][start_col] in '|F7':
-            direction = (-1,0)
-            return (start_row,start_col), direction
-    if start_row < rows-1:        
-        if grid[start_row+1][start_col] in '|LJ':
-            direction = (1,0)
-            return (start_row,start_col), direction
-    if start_col > 0:
-        if grid[start_row][start_col-1] in '-LF':
-            direction = (0,-1)
-            return (start_row,start_col), direction
-    if start_col < collumns-1:        
-        if grid[start_row][start_col+1] in '-J7':
-            direction = (0,1)
-            return (start_row,start_col), direction
-
-def link_pipes(grid, start_pos, direction):
-    pipe_path = [start_pos]
-    r, c = [sum(x) for x in zip(start_pos, direction)]
-    pos = grid[r][c]
     
-    steps = 1
-    while not pos =='S':
-        new_dir = [sum(x) for x in zip(pipes[pos], direction)]
-        direction = [x//abs(x) if not x == 0 else 0 for x in new_dir ]
+    start_directions = {(-1,0): '|F7', (1,0): '|LJ', (0,-1): '-LF', (0,1): '-J7'}
 
-        r, c = [sum(x) for x in zip((r,c), direction)]
-        pos = grid[r][c]
+    for d,pipes in start_directions.items():
+        pipe_pos = tuple(sum(x) for x in zip(pos, d))
+        pipe = grid[pipe_pos]
+        if (0 < pipe_pos[0] < rows-1) and (0 < pipe_pos[1] < collumns-1):
+            if pipe in pipes:
+                return pos, d
+
+
+def link_pipes(grid, pos, direction):
+    '''Trace the path of the pipe untill back at the start position'''
+    pipe, steps = None,0
+    while not pipe =='S':
+        pos = tuple(sum(x) for x in zip(pos, direction))
+        pipe = grid[pos]
+
+        direction = [sum(x) for x in zip(pipes[pipe], direction)]
         steps += 1
-        # pipe_path.append (r,c)
 
     return steps//2
+
 
 def part1():
     start_pos, direction = find_start(input)
     return (link_pipes(input, start_pos, direction))
-
 
 def part2():
     return None
